b5d52d6e1c0e8b7bedc3da69469d21a7c5a2f6cc
fuzail <fuzailansarisecret@gmail.com>
2025-06-23 15:41:56 +0530

    imagekit

---
+++ app/api/files/[fileId]/delete/route.ts
@@ -0,0 +1,1 @@
+// TODO: have to write the code to delete a file
---
+++ app/api/files/upload/route.ts
@@ -0,0 +1,109 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import ImageKit from "imagekit";
+import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
+
+const imageKit = new ImageKit({
+  publicKey: (process.env.IMAGEKIT_PUBLIC_KEY as string) || "",
+  privateKey: (process.env.IMAGEKIT_PRIVATE_KEY as string) || "",
+  urlEndpoint: (process.env.IMAGEKIT_URL_ENDPOINT as string) || "",
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    // Check auth
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const formData = await req.formData();
+    // Grab the data from fromData
+    const file = formData.get("file") as File;
+    const formUserId = formData.get("userId") as string;
+    const formParentId = formData.get("parentId") as string;
+
+    if (formUserId !== userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    if (!file) {
+      return NextResponse.json(
+        { error: "No file was provided" },
+        { status: 401 }
+      );
+    }
+
+    // if file exist then
+    if (formParentId) {
+      const [parentFolder] = await db
+        .select()
+        .from(files)
+        .where(
+          and(
+            eq(files.id, formParentId),
+            eq(files.userId, formUserId),
+            eq(files.isFolder, true)
+          )
+        );
+    }
+    if (!formParentId) {
+      return NextResponse.json(
+        { error: "Parent folder is not found" },
+        { status: 404 }
+      );
+    }
+
+    if (!file.type.startsWith("images/") && file.type !== "application/pdf") {
+      return NextResponse.json(
+        {
+          error: "Only images and pdf are allowed to upload",
+        },
+        { status: 403 }
+      );
+    }
+
+    // we need to convert it to the buffer before uploading it.
+    const buffer = await file.arrayBuffer();
+    const fileBuffer = Buffer.from(buffer);
+    const folderPath = formParentId
+      ? `/dropfile/${userId}/folder/${formParentId}`
+      : `/dropfile/${userId}`;
+
+    const originalFileName = file.name;
+    const fileExtention = originalFileName.split(".").pop() || "";
+    const uniqueFileName = `${uuidv4()}.${fileExtention}`;
+
+    const response = await imageKit.upload({
+      file: fileBuffer,
+      fileName: uniqueFileName,
+      folder: folderPath,
+      useUniqueFileName: false,
+    });
+
+    const fileData = {
+      name: response.name,
+      path: response.filePath,
+      size: response.size,
+      type: response.fileType,
+      fileUrl: response.url,
+      thumbnailUrl: response.thumbnailUrl,
+      userId: userId,
+      parentId: formParentId,
+      isFolder: false,
+      isStarred: false,
+      isTrashed: false,
+    };
+
+    const newFile = await db.insert(files).values(fileData).returning();
+
+    return NextResponse.json(newFile);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "failed to upload file" },
+      { status: 500 }
+    );
+  }
+}
